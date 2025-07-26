af89ce5e7f18e8b448213279e5e9f9a2ec32f6bf
Geoffrey Wells <Gwells625@gmail.com>
2025-07-26 12:38:50 -0400

    Add inline data.

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -7,6 +7,9 @@ namespace UnitTestingExercise.Tests
     {
         [Theory]
         [InlineData(2, 3, 5, 10)] //Add test data <-------
+        [InlineData(6, 7, 2, 15)]
+        [InlineData(4, 9, 1, 14)]
+        [InlineData(7, 2, 3, 12)]
         public void AddTest(int num1, int num2, int num3, int expected)
         {
             //Start Step 3 here:
@@ -28,6 +31,8 @@ namespace UnitTestingExercise.Tests
         [Theory]
         [InlineData(5,5,0)]
         [InlineData(5,10,-5)]//Add test data <-------
+        [InlineData(4,12,-8)]
+        [InlineData(6, 3, 3)]
         public void SubtractTest(int minuend, int subtrhend, int expected)
         {
             //Start Step 5 here:
@@ -46,6 +51,8 @@ namespace UnitTestingExercise.Tests
         [Theory]
         [InlineData(2,2,4)]//Add test data <------
         [InlineData(6,5,30)]
+        [InlineData(5,2,10)]
+        [InlineData(3,3,9)]
         public void MultiplyTest(int num1, int num2, int expected)
         {
             //Start Step 7 here:
@@ -64,6 +71,8 @@ namespace UnitTestingExercise.Tests
         [Theory]
         [InlineData(6,3,2)]//Add test data <------
         [InlineData(10,5,2)]
+        [InlineData(20,5,4)]
+        [InlineData(30,5,6)]
         public void DivideTest(int num1, int num2, int expected)
         {
             //Arrange
